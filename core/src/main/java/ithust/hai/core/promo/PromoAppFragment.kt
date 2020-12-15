02b6c6110a8f3d3d73c60f69760e55584b4be57d
CongHai <conghai.luong@foodycorp.vn>
2020-12-15 16:45:35 +0700

    [Core] Add support back in promo fragment

--- core/src/main/java/ithust/hai/core/promo/PromoAppFragment.kt
+++ core/src/main/java/ithust/hai/core/promo/PromoAppFragment.kt
@@ -2,9 +2,13 @@ package ithust.hai.core.promo
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import ithust.hai.core.R
 import ithust.hai.core.base.BaseBindingFragment
 import ithust.hai.core.databinding.CoreFragmentPromoAppBinding
+import ithust.hai.core.extension.back
 import ithust.hai.core.extension.initVertical
+import ithust.hai.core.utils.ExtraKey
 import ithust.hai.core.utils.StoreUtil
 
 /**
@@ -28,10 +32,22 @@ class PromoAppFragment : BaseBindingFragment<CoreFragmentPromoAppBinding>() {
     }
 
     override fun initUI() {
+        if (requireArguments().getBoolean(ExtraKey.A)) {
+            binding.toolbar.setNavigationIcon(R.drawable.core_ic_back)
+            binding.toolbar.setNavigationOnClickListener {
+                back()
+            }
+        }
         binding.rvAppList.apply {
             initVertical(context)
             adapter = this@PromoAppFragment.adapter
         }
         adapter.setData(appList)
+    }
+
+    companion object {
+        fun newInstance(isShowBackIcon: Boolean) = PromoAppFragment().apply {
+            arguments = bundleOf(ExtraKey.A to isShowBackIcon)
+        }
     }
 }
