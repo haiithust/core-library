7d829eeb6edfb942ac32cd577320507c1bb90aa7
Hai Luong <lh@mega.co.nz>
2026-04-09 14:48:36 +0700

    Fix build for AGP 9.x, Kotlin 2.x, Gradle 9.x
    - Migrate from buildscript/apply to plugins DSL
    - Replace jcenter() with mavenCentral() via settings.gradle
    - Remove deprecated android-maven-gradle-plugin (maven-publish already used)
    - Remove deprecated kotlin-kapt, kotlin-stdlib, fileTree, jetifier
    - Update core module dependencies to latest versions
    - Bump compileSdk/targetSdk to 35

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
     }
 
     buildTypes {
@@ -35,41 +35,34 @@ android {
     }
 
     kotlinOptions {
-        val options = this
-        options.jvmTarget = "17"
+        jvmTarget = "17"
     }
 
     resourcePrefix("core_")
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.21")
     implementation("com.jakewharton.timber:timber:5.0.1")
 
-    implementation("androidx.activity:activity-ktx:1.9.3")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation("androidx.core:core-ktx:1.18.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.1")
 
     implementation("com.google.android.play:review-ktx:2.0.2")
     implementation("com.google.android.play:app-update-ktx:2.1.0")
     implementation("javax.inject:javax.inject:1")
 }
-repositories {
-    mavenCentral()
-}
 
 afterEvaluate {
     publishing {
         publications {
-            // Creates a Maven publication called "release".
             create<MavenPublication>("release") {
                 from(components["release"])
                 groupId = "com.github.haiithust"
